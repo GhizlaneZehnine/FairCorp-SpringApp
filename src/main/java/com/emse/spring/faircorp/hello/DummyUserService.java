2fbbf4a5ba435f8f7c0f596d364bb8df5d9e6d69
Ghizlane <ghizlane.zehnine@etu.emse.fr>
2020-11-25 10:56:22 +0100

    initial commit

---
+++ src/main/java/com/emse/spring/faircorp/hello/DummyUserService.java
@@ -0,0 +1,32 @@
+package com.emse.spring.faircorp.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DummyUserService implements UserService {
+
+    private GreetingService greetingService;
+    private ArrayList<String> users;
+
+    @Autowired
+    public DummyUserService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+        this.users = new ArrayList<String>();
+        this.users.add("Charles");
+        this.users.add("Elodie");
+    }
+
+    public GreetingService getGreetingService() {
+        return greetingService;
+    }
+
+    @Override
+    public void greetAll() {
+        for(int i=0; i < users.size() ; i++)
+            this.greetingService.greet(users.get(i));
+    }
+}
